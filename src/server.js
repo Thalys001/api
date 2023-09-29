c5fa18c5c7dce9c241a667bc776881fad32d48eb
Thalys001 <thalyssouza708@gmail.com>
2023-09-28 22:29:47 -0300

    feat: adding query params

--- src/server.js
+++ src/server.js
@@ -7,5 +7,10 @@ app.get("/message/:id/:user", (request, response) => {
   response.send(`id da mensagem: ${id}. para o usuário: ${user}`);
 });
 
+app.get("/users", (request, response) => {
+  const { page, limit, title } = request.query;
+  response.send(`Página: ${page}, Mostrar: ${limit}, Titulo: ${title}`);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
