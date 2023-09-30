d3b28c0b5a1f7ffe5cbedb9a6b1d923d76b6e111
Thalys001 <thalyssouza708@gmail.com>
2023-09-30 18:35:33 -0300

    feat: estruturando rotas

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 
+const routes = require("./routes");
+
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
-  response.send(`id da mensagem: ${id}. para o usuário: ${user}`);
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit, title } = request.query;
-  response.send(`Página: ${page}, Mostrar: ${limit}, Titulo: ${title}`);
-});
+app.use(routes)
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
