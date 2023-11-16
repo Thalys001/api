5f9a608937cf1fa5c60aa1ffada87020f66f6de7
Thalys001 <thalyssouza708@gmail.com>
2023-11-15 22:09:43 -0300

    feat: handling errors

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,11 @@
+const AppError = require("../utils/AppError");
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    if (!name) {
+      throw new AppError("Nome é obrigatório");
+    }
 
     response.json({ name, email, password });
   }
